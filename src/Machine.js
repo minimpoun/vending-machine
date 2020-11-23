ee5db0487bcb119c8b1e6cbaacde522cce54dc81
minimpoun <cdringenberg1@gmail.com>
2020-11-23 16:53:18 -0600

    init commit and added tests for deposit and selectItem

--- src/Machine.js
+++ src/Machine.js
@@ -1,18 +1,102 @@
-class Machine {
-  constructor() {
-    this.snacks = []
+class Machine 
+{
+  constructor() 
+  {
+    this.snacks = [];
+    this.billsAccpted = [10, 20, 50, 100, 500];
+    this.depositedMoney = 0;
+    this.itemList = [];
   }
 
-  seeSelections() {
-    return this.snacks
+  seeSelections() 
+  {
+    return this.snacks;
   }
 
-  stock(inventory) {
-    if(inventory == undefined) {
-      throw Error("please do not troll. you cannot stock nothing.")
+  stock(inventory) 
+  {
+    if(inventory == undefined) 
+    {
+      throw Error("please do not troll. you cannot stock nothing.");
     }
-    this.snacks = inventory
+    this.snacks = inventory;
+  }
+
+  deposit(amtDeposited)
+  {
+    if(!this.billsAccpted.includes(amtDeposited))
+    {
+      throw Error('This machine only accepts ["10", "20", "50", "100", "500"] amounts');
+    }
+    amtDeposited += this.depositedMoney;
+    return `You have deposited Rs ${amtDeposited}`;
+  }
+
+  selectItem(code)
+  {
+    const item = this.itemList.filter((element) => 
+    {
+      return element.id === code;
+    });
+
+    if (item[0].amount === 0)
+    {
+      return "The item you selected is unavailable"
+    }
+
+    if (item[0].item.price > this.depositedMoney)
+    {
+      const amtNeeded = Math.abs(item[0].item.price - this.depositedMoney);
+      return `You have insufficient funds. Please add Rs ${amtNeeded}.`;
+    }
+
+    
   }
 }
 
-module.exports = Machine
+module.exports = Machine;
+
+/*
+As a customer, I want to know how much money I have deposited, so that I know what I can purchase.
+- Given I am using the vending machine, 
+- when I insert money, 
+- then I see the total I have deposited on a screen. 
+- `deposit(100)` returns `'You have deposited Rs 100'`
+- The machine should accept bills in these amounts: `10, 20, 50, 100, 500`
+
+As a customer, I want to add additional money, so that I can use the denominations I have to purchase an item.
+- Given I have deposited money in the vending machine,
+- when I deposit additional money,
+- then I see the new total on a screen. 
+- After depositing Rs 100, `deposit(50)` returns `'You have deposited Rs 150'`
+
+As a customer, I want to see a message if my item is unavailable, so that I can make another choice.
+- Given I am using the vending machine, 
+- when I enter a code for an item that is unavailable, 
+- then I see a message that the item is unavailable.
+- `selectItem(code)` returns `'The item you selected is unavailable'`
+
+As a customer, I want to see a message if my deposit is insufficient, so that I know to add more money.
+- Given I have made a choice, 
+- when I have not deposited enough money for that item, 
+- then I see a message telling me how much more to deposit.
+- `selectItem(code)` returns `'Your deposit is insufficient.  Please add Rs 20 for this item'`
+
+As a customer, I want to receive change, so that I don’t pay more than the item costs.
+- Given I have made a selection, 
+- when the item is delivered, 
+- then I receive correct change (in correct monetary units)
+- `selectItem(code)` returns an object with the item and an array of bills `{item: 'mints', change: [20, 10]}`
+
+As a customer, I want to receive my money back when I push the cancel button, so that I can change my mind.
+- Given that I have deposited money,
+- When I push the cancel button,
+- Then I receive my money back
+- `cancel()` returns `{change: [100]}`
+
+As a customer, I want to know if the vending machine can make change, so that I can cancel my choice if it can't make change.
+- Given I have deposited money and selected a choice, 
+- when the machine does not have correct change, 
+- then I see a message
+- `selectItem(code)` returns `'Cannot return proper change.  Please choose another item or cancel the transaction'`
+*/
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -1,31 +1,37 @@
-const Machine = require('../src/Machine')
+const Machine = require('../src/Machine');
 
-describe('The vending machine', () => {
-  it('is initialized with no items', () => {
-    // SEAT
-    // setup
-    const vendingMachine = new Machine();
+describe('Vending Machine', () => 
+{
+  describe('Stocking Items', () =>
+  {
+    it('is initialized with no items', () => 
+    {
+      // SEAT
+      // setup
+      const vendingMachine = new Machine();
 
-    // exercise & assert
-    expect(vendingMachine.seeSelections()).toEqual([])
+      // exercise & assert
+      expect(vendingMachine.seeSelections()).toEqual([]);
 
-    // teardown, not needed
-  })
+      // teardown, not needed
+    });
 
-  it('can stock one snack', () => {
-    // setup
-    const vendingMachine = new Machine();
-    const snack = {
-      name: 'macadamia nuts',
-      price: 250
-    }
+    it('can stock one snack', () => 
+    {
+      // setup
+      const vendingMachine = new Machine();
+      const snack = {
+        name: 'macadamia nuts',
+        price: 250
+      };
 
-    // exercise
-    vendingMachine.stock([snack])
+      // exercise
+      vendingMachine.stock([snack]);
 
-    // assert
-    expect(vendingMachine.seeSelections()).toEqual([snack])
-  })
+      // assert
+      expect(vendingMachine.seeSelections()).toEqual([snack]);
+    });
+  });
 
   // someone attempts to stock without inventory
   // it('stocks nothing if there is no inventory passed', () => {
@@ -39,12 +45,99 @@ describe('The vending machine', () => {
   //   expect(vendingMachine.seeSelections()).toEqual([])
   // })
 
-  it('displays an error if no inventory comes with stocking', () => {
+  it('displays an error if no inventory comes with stocking', () => 
+  {
     // setup
-    const vendingMachine = new Machine()
-    const displayMessage = "please do not troll. you cannot stock nothing."
+    const vendingMachine = new Machine();
+    const displayMessage = "please do not troll. you cannot stock nothing.";
 
     // exercise & assert
-    expect(() => vendingMachine.stock()).toThrow(displayMessage)
+    expect(() => vendingMachine.stock()).toThrow(displayMessage);
   })
-})
+
+  describe('Inserting Money', () => {
+  
+    it('displays how much money was inserted', () => 
+    {
+      const vendingMachine =  new Machine();
+      const money = 100;
+      expect(vendingMachine.deposit(money)).toEqual('You have deposited Rs 100');
+    });
+    
+    it('only accepts bills in ["10", "20", "50", "100", "500"] amounts', () =>
+    {
+      const vendingMachine = new Machine();
+      const money = 15;
+      expect(() => vendingMachine.deposit(money)).toThrow('This machine only accepts ["10", "20", "50", "100", "500"] amounts');
+    });
+
+    it('contains a variable called depositedMoney', () =>
+    {
+      const vendingMachine = new Machine();
+      expect(vendingMachine.depositedMoney).toBeDefined();
+    });
+
+    it('depositedMoney should be a int', () =>
+    {
+      const vendingMachine = new Machine();
+      expect(typeof(vendingMachine.depositedMoney) === 'number');
+    });
+
+    it('adds additional money to existing credit', () => 
+    {
+      let vendingMachine = new Machine();
+      vendingMachine.depositedMoney = 100;
+      expect(vendingMachine.deposit(50)).toEqual('You have deposited Rs 150');
+    });
+  });
+
+  describe('selecting item', () =>
+  {
+    it ('should not allow buying an unavailable item', () =>
+    {
+      let vendingMachine = new Machine();
+      vendingMachine.itemList = 
+      [
+        {
+          id: "E7", 
+          item: 
+          {
+            name: "chips", 
+            price: 250
+          }, 
+          amount: 0
+        }
+      ];
+
+      const item = "E7";
+      expect(vendingMachine.selectItem(item)).toEqual("The item you selected is unavailable");
+    });
+
+    it ('checks for insufficient funds for item', () =>
+    {
+      let vendingMachine = new Machine();
+      vendingMachine.itemList = 
+      [
+        {
+          id: "E7", 
+          item: 
+          {
+            name: "chips", 
+            price: 250
+          }, 
+          amount: 1
+        }
+      ];
+      const item = "E7";
+
+      const amtNeed = Math.abs(vendingMachine.itemList[0].item.price - vendingMachine.depositedMoney);
+      expect(vendingMachine.selectItem(item)).toEqual(`You have insufficient funds. Please add Rs ${amtNeed}.`);
+    });
+
+    // it('returns correct change of item purchased', () =>
+    // {
+    //   let vendingMachine = new Machine();
+
+    // })
+  });
+});
